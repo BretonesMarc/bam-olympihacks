08425c5fc745d01d07af2ef64f395a3a6c2e9957
Marc <marcguybretones@gmail.com>
2023-05-21 12:49:24 -0400

    Retreive old smart contract from history

--- Solidity/Solidity_smartContract.sol
+++ Solidity/Solidity_smartContract.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.6.0 <0.9.0;
 
-contract CompanyFactory{
+contract CompanyFactory {
+    address public ownerOfContract;
+    mapping(address => bool) public authorizedCompanies;
+
     struct Partners {
         uint id;
         string visibility;
@@ -12,58 +15,75 @@ contract CompanyFactory{
         string location;
         address owner;
         uint id;
-
+        string uRL;
         // Number of iso certificates
         uint certificatesNumber;
         mapping(uint => string) certificates;
-
         uint partnersNumber;
         mapping(uint => Partners) partners;
+        uint statisticsNumber;
+        mapping(uint => uint) statistics;
     }
 
-    uint public numberOfCompanies = 0;
+    uint numberOfCompanies = 0;
     mapping(uint => Company) companies;
+
     mapping(address => uint) public userCompanies;
 
-    Company[] public newCompanies;
-
-    function getAddressesWithCompanies() public view returns (address[] memory) {
-        uint count = 0;
-
-        // Count the number of addresses with companies
-        for (uint i = 0; i < numberOfCompanies; i++) {
-            if (userCompanies[companies[i].owner] == i) {
-                count++;
-            }
-        }
-
-        // Create an array to store the addresses
-        address[] memory addressesWithCompanies = new address[](count);
-
-        // Populate the array with addresses
-        uint index = 0;
-        for (uint i = 0; i < numberOfCompanies; i++) {
-            if (userCompanies[companies[i].owner] == i) {
-                addressesWithCompanies[index] = companies[i].owner;
-                index++;
-            }
-        }
-
-        return addressesWithCompanies;
+    constructor() {
+        ownerOfContract = msg.sender;
     }
 
-    // Create a new company
-    function createCompany(string memory _name, string memory _location) public {
-        uint companyID = numberOfCompanies;
+    // modifier onlyOwner() {
+    //     require(
+    //         msg.sender == ownerOfContract,
+    //         "Only the contract owner can call this function"
+    //     );
+    //     _;
+    // }
 
-        Company storage newCompany = companies[companyID];
+    // modifier onlyAuthorizedCompany() {
+    //     require(
+    //         authorizedCompanies[msg.sender] == true,
+    //         "Only authorized companies can call this function"
+    //     );
+    //     _;
+    // }
+
+    // function addAuthorizedCompany(address company) external {
+    //     authorizedCompanies[company] = true;
+    // }
+
+    // function removeAuthorizedCompany(address company) external {
+    //     authorizedCompanies[company] = false;
+    // }
+
+    // Create a new company
+    function createCompany(
+        string memory _name,
+        string memory _location,
+        string memory _uRL
+    ) public {
+        Company storage newCompany = companies[numberOfCompanies];
         newCompany.name = _name;
         newCompany.location = _location;
         newCompany.owner = msg.sender;
-        newCompany.id = companyID;
+        newCompany.id = numberOfCompanies;
+        newCompany.uRL = _uRL;
 
-        userCompanies[msg.sender] = companyID; // Stocker l'ID de la compagnie pour cet utilisateur
-        numberOfCompanies = numberOfCompanies + 1;
+        userCompanies[msg.sender] = numberOfCompanies; // Stocker l'ID de la compagnie pour cet utilisateur
+        numberOfCompanies = numberOfCompanies++;
+    }
+
+    function getVisibilityLevel(
+        uint _companyID
+    ) public view returns (Partners[] memory) {
+        Company storage company = companies[_companyID];
+        Partners[] memory partners = new Partners[](company.partnersNumber);
+        for (uint i = 0; i < company.partnersNumber; i++) {
+            partners[i] = company.partners[i];
+        }
+        return partners;
     }
 
     function addCertifications(string memory _iso) public {
@@ -71,12 +91,34 @@ contract CompanyFactory{
 
         Company storage company = companies[_companyID];
         company.certificates[company.certificatesNumber++] = _iso;
+    }
 
+    function addStatistics(uint _statistics) public {
+        uint _companyID = userCompanies[msg.sender]; // Utiliser l'adresse du message pour trouver l'ID de la compagnie associée
+
+        Company storage company = companies[_companyID];
+        company.statistics[company.statisticsNumber++] = _statistics;
+    }
+
+    function getStatistics() public view returns (uint[] memory) {
+        uint _companyID = 0;
+        for (uint i = 0; i < numberOfCompanies; i++) {
+            if (userCompanies[companies[i].owner] == i) {
+                _companyID = companies[i].id;
+                break;
+            }
+        }
+        Company storage company = companies[_companyID];
+        uint[] memory statistics = new uint[](company.statisticsNumber);
+        for (uint i = 0; i < company.statisticsNumber; i++) {
+            statistics[i] = company.statistics[i];
+        }
+        return statistics;
     }
 
     function getCertifications() public view returns (string[] memory) {
         uint _companyID = 0;
-         for (uint i = 0; i < numberOfCompanies; i++) {
+        for (uint i = 0; i < numberOfCompanies; i++) {
             if (userCompanies[companies[i].owner] == i) {
                 _companyID = companies[i].id;
                 break;
@@ -90,45 +132,68 @@ contract CompanyFactory{
         return certificates;
     }
 
-    function getPartners(uint _companyID) public view returns(Partners[] memory) {
+    function getPartners() public view returns (Partners[] memory) {
+        uint _companyID = 0;
+        for (uint i = 0; i < numberOfCompanies; i++) {
+            if (userCompanies[companies[i].owner] == i) {
+                _companyID = companies[i].id;
+                break;
+            }
+        }
+
         Company storage company = companies[_companyID];
         Partners[] memory partners = new Partners[](company.partnersNumber);
-        for (uint i = 0; i < company.certificatesNumber; i++) {
+        for (uint i = 0; i < company.partnersNumber; i++) {
             partners[i] = company.partners[i];
         }
         return partners;
     }
 
-    // function getCompany() public view returns (string memory name, string memory location, uint certificatesNumber, address) {
-    //     uint _companyID = userCompanies[msg.sender]; // Utiliser l'adresse du message pour trouver l'ID de la compagnie associée
-        
-        
-    //     Company storage company = companies[_companyID];
-    //     return (company.name, company.location, company.certificatesNumber, msg.sender);
-    // }
+    function compareStrings(
+        string memory a,
+        string memory b
+    ) private pure returns (bool) {
+        return (keccak256(abi.encodePacked(a)) ==
+            keccak256(abi.encodePacked(b)));
+    }
 
-    function getCompany(
-        uint _companyID
-    )
+    function getCompany()
         public
         view
         returns (
             string memory name,
             string memory location,
-            uint certificatesNumber
+            uint certificatesNumber,
+            address,
+            string memory uRL
         )
     {
-        _companyID = userCompanies[msg.sender]; // Utiliser l'adresse du message pour trouver l'ID de la compagnie associée
+        uint _companyID = userCompanies[msg.sender]; // Utiliser l'adresse du message pour trouver l'ID de la compagnie associée
+
         Company storage company = companies[_companyID];
-        return (company.name, company.location, company.certificatesNumber);
+        return (
+            company.name,
+            company.location,
+            company.certificatesNumber,
+            msg.sender,
+            company.uRL
+        );
     }
 
-
-
-      function getCompanies() public view returns (string[] memory, address[] memory, uint[] memory) {
+    function getCompanies()
+        public
+        view
+        returns (
+            string[] memory,
+            address[] memory,
+            uint[] memory,
+            string[] memory
+        )
+    {
         string[] memory names = new string[](numberOfCompanies);
         address[] memory ownersList = new address[](numberOfCompanies);
         uint[] memory ids = new uint[](numberOfCompanies);
+        string[] memory uRLs = new string[](numberOfCompanies);
 
         for (uint i = 0; i < numberOfCompanies; i++) {
             names[i] = companies[i].name;
@@ -136,15 +201,36 @@ contract CompanyFactory{
             ids[i] = companies[i].id;
         }
 
-        return (names, ownersList, ids);
+        return (names, ownersList, ids, uRLs);
     }
 
-    function addPartner(uint _myCompanyID, uint _companyID) public {
+    function addPublicPartner(uint _myCompanyID, uint _companyID) public {
         Company storage c = companies[_myCompanyID];
-        c.partners[c.partnersNumber++] = Partners({id: _companyID, visibility: "public"});
+        c.partners[c.partnersNumber++] = Partners({
+            id: _companyID,
+            visibility: "public"
+        });
     }
 
-    function seeRequests(uint _myCompanyID) public view returns (uint[] memory) {
+    function addTransparentPartner(uint _myCompanyID, uint _companyID) public {
+        Company storage c = companies[_myCompanyID];
+        c.partners[c.partnersNumber++] = Partners({
+            id: _companyID,
+            visibility: "transparent"
+        });
+    }
+
+    function addPrivatePartner(uint _myCompanyID, uint _companyID) public {
+        Company storage c = companies[_myCompanyID];
+        c.partners[c.partnersNumber++] = Partners({
+            id: _companyID,
+            visibility: "private"
+        });
+    }
+
+    function seeRequests(
+        uint _myCompanyID
+    ) public view returns (uint[] memory) {
         uint[] memory companyRequests;
         uint count = 0;
 
@@ -176,5 +262,60 @@ contract CompanyFactory{
         }
 
         return companyRequests;
+    }
+
+    function getPartnerInfo(
+        uint _companyID,
+        uint _partnerIndex
+    ) public view returns (string[] memory, Partners[] memory, uint[] memory) {
+        Company storage company = companies[_companyID];
+        require(
+            _partnerIndex < company.partnersNumber,
+            "Invalid partner index"
+        );
+
+        Partners storage partner = company.partners[_partnerIndex];
+
+        if (compareStrings(partner.visibility, "public")) {
+            // Return certificates only
+            return (getCertifications(), new Partners[](0), new uint[](0));
+        } else if (compareStrings(partner.visibility, "transparent")) {
+            // Return certificates and partners
+            return (getCertifications(), getPartners(), new uint[](0));
+        } else if (compareStrings(partner.visibility, "private")) {
+            // Return certificates, partners, and statistics
+            return (getCertifications(), getPartners(), getStatistics());
+        } else {
+            revert("Invalid visibility level");
+        }
+    }
+
+    function getAddressesWithCompanies()
+        public
+        view
+        returns (address[] memory)
+    {
+        uint count = 0;
+
+        // Count the number of addresses with companies
+        for (uint i = 0; i < numberOfCompanies; i++) {
+            if (userCompanies[companies[i].owner] == i) {
+                count++;
+            }
+        }
+
+        // Create an array to store the addresses
+        address[] memory addressesWithCompanies = new address[](count);
+
+        // Populate the array with addresses
+        uint index = 0;
+        for (uint i = 0; i < numberOfCompanies; i++) {
+            if (userCompanies[companies[i].owner] == i) {
+                addressesWithCompanies[index] = companies[i].owner;
+                index++;
+            }
+        }
+
+        return addressesWithCompanies;
     }
 }
